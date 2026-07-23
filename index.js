3645a6ef5c640a547a50b057aff4501757e6c0da
afeefalmasud <its.afeefalmasud@gmail.com>
2026-07-23 19:28:34 +0600

    server created

---
+++ index.js
@@ -0,0 +1,80 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db('sportnest');
+    const facilityCollection = db.collection('facilities');
+
+    app.post('/facility', async(req,res)=>{
+      const facilityData = req.body;
+      const result = await facilityCollection.insertOne(facilityData);
+      res.json(result);
+    })
+
+    app.get('/facility', async(req,res)=>{
+      const result = await facilityCollection.find().toArray();
+      res.json(result);
+    })
+
+    app.get('/facility/:id', async(req,res)=>{
+      const {id} = req.params;
+      const result = await facilityCollection.findOne({_id: new ObjectId(id)})
+      res.json(result);
+    })
+
+    app.patch('/facility/:id', async(req,res)=>{
+      const {id} = req.params;
+      const updatedData = req.body;
+      const result = await facilityCollection.updateOne(
+        {_id: new ObjectId(id)},
+        {$set: updatedData}
+      )
+      res.json(result);
+    })
+
+    app.delete('/facility/:id', async(req,res)=>{
+      const {id} = req.params;
+      const result = await facilityCollection.deleteOne({_id: new ObjectId(id)});
+      res.json(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req,res)=>{
+    res.send('Server is running fine');
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+})
