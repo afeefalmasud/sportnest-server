d33105f9292eb5864d0de42339eeeb0b8e453170
afeefalmasud <its.afeefalmasud@gmail.com>
2026-07-26 00:26:44 +0600

    fixing 3

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
 
     app.get('/facility/:id', async(req,res)=>{
       const {id} = req.params;
-      const resul = await facilityCollection.findOne({_id: new ObjectId(id)})
+      const resu = await facilityCollection.findOne({_id: new ObjectId(id)})
       res.json(result);
     })
 
